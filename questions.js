708e30f2cb9419b22672c8e8182dc5310aecedd2
Poorviga R <56013004+Poorviga@users.noreply.github.com>
2024-07-26 20:57:21 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which of the following statements about Java is true?",
+        answer: "C. Java programs are executed by the Java Virtual Machine (JVM)",
+        options: [
+            "A. Java programs are compiled directly into machine code",
+            "B. Java supports multiple inheritance",
+            "C. Java programs are executed by the Java Virtual Machine (JVM)",
+            "D. Java does not support object-oriented programming"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the keyword used to define a subclass in Java?",
+        answer: "A. final",
+        options: [
+            "A. final",
+            "B. static",
+            "C. const",
+            "D. override"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the default value of a boolean variable in Java if it is not explicitly initialized?",
+        answer: "B. false",
+        options: [
+            "A. true",
+            "B. false",
+            "C. 0",
+            "D. null"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which of the following is not a valid access specifier for a top-level class in Java?",
+        answer: "B. protected",
+        options: [
+            "A. private",
+            "B. protected",
+            "C. default (no specifier)",
+            "D. final"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which of the following collections in Java guarantees the order of elements and allows duplicate elements?",
+        answer: "C. ArrayList",
+        options: [
+            "A. HashSet",
+            "B. TreeMap",
+            "C. ArrayList",
+            "D. LinkedHashMap"
+        ]
+    }
+];
